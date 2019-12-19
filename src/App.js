e1ec0b83985e5d772a7e220573e9927ad426a98c
Jorge Fernandes <jorge.fernandes.dev@gmail.com>
2019-12-19 16:44:59 +0000

    Lesson 36

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 import './App.css';
 
@@ -33,7 +34,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input type="search" placeholder="search monsters" onChange={ e => this.setState({searchField: e.target.value}) }></input>
+        <SearchBox placeholder="search monsters" handleChange={ e => this.setState({searchField: e.target.value}) }></SearchBox>
         <CardList monsters={ filteredMonsters } />
       </div>
     );
