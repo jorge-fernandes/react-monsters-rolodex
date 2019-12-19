ba7cdf0236bac2569e3962c1f135657bea74d003
Jorge Fernandes <jorge.fernandes.dev@gmail.com>
2019-12-19 10:11:55 +0000

    Lesson 22

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,21 +6,29 @@ class App extends Component {
     super();
 
     this.state = {
-      welcomeLabel: 'Changed src/App.js to class instead of function.'
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: 'asc1'
+        }, {
+          name: 'Dracula',
+          id: 'asc2'
+        }, {
+          name: 'Zombie',
+          id: 'asc3'
+        }
+      ]
     }
   }
   render () {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {this.state.welcomeLabel}
-          </p>
-          <button onClick={() => this.setState({welcomeLabel: 'Also added state.'})}>
-            Change text!
-          </button>
-        </header>
+        {
+          this.state.monsters.map(monster => (
+            <h1 key={ monster.id}> { monster.name } </h1>
+            )
+          )
+        }
       </div>
     );
   }
