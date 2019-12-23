c2d739d57efe057369825f662707b3fb116c5ccb
Jorge Fernandes <jorge.fernandes.dev@gmail.com>
2019-12-23 12:09:22 +0000

    Updates

---
+++ precache-manifest.f5004024c0f2f17be5c1bc8d06dbcbc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6233bc66258755623f79e6dc5e405bc1",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4596053c1afc16475ac4",
+    "url": "/react-monsters-rolodex/static/css/main.da83c279.chunk.css"
+  },
+  {
+    "revision": "ba6022be9619d47669ca",
+    "url": "/react-monsters-rolodex/static/js/2.8b970949.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-monsters-rolodex/static/js/2.8b970949.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4596053c1afc16475ac4",
+    "url": "/react-monsters-rolodex/static/js/main.6dba0113.chunk.js"
+  },
+  {
+    "revision": "6e7fdd83c2267585592c",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.4474d36d.js"
+  }
+]);
