7e4c1ade8c64a3b35e9ffd54640ef341e25a7b2e
Jorge Fernandes <jorge.fernandes.dev@gmail.com>
2019-12-23 11:46:36 +0000

    Lesson 038

--- src/App.js
+++ src/App.js
@@ -28,13 +28,17 @@ class App extends Component {
     }
   }
 
+  handleChange = e => {
+    this.setState({searchField: e.target.value});
+  }
+
   render () {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
 
     return (
       <div className="App">
-        <SearchBox placeholder="search monsters" handleChange={ e => this.setState({searchField: e.target.value}) }></SearchBox>
+        <SearchBox placeholder="search monsters" handleChange={ this.handleChange }></SearchBox>
         <CardList monsters={ filteredMonsters } />
       </div>
     );
